53b090ec98c5e66f2fd1566ef9457eace8ef3d16
gguerrero11 <gguerrero11@gmail.com>
2015-03-23 17:41:22 -0600

    Initial Commit

---
+++ Snipey/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Snipey
+//
+//  Created by Gabriel Guerrero on 3/23/15.
+//  Copyright (c) 2015 Gabe Guerrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Snipey/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Snipey
+//
+//  Created by Gabriel Guerrero on 3/23/15.
+//  Copyright (c) 2015 Gabe Guerrero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
